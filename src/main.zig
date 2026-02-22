c3b33ba94be97121d9ed105dc7424e4f95196958
Cole Leavitt <cole@unwrap.rs>
2026-02-22 10:05:54 -0700

    Empty base for full review

--- src/main.zig
+++
@@ -1,6 +0,0 @@
-const std = @import("std");
-const lex = @import("lexer/js_lexer.zig");
-
-pub fn main() anyerror!void {
-    std.log.info("All your codebase are belong to us. {s}", .{lex.Keywords.get("hey")});
-}
